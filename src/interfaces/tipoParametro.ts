b54c2db7ffc4b4ae060d3e1d9b1ef0acb624fb03
Gustavo Muraoka Silva <gustavomuraoka15@gmail.com>
2025-09-26 20:23:08 -0300

    AT-6 feat: Criação da Rota de Armazenamento

---
+++ src/interfaces/tipoParametro.ts
@@ -0,0 +1,11 @@
+export interface TipoParametro {
+    pk: number;
+    json_id: string;
+    nome: string;
+    tipo: string;
+    offset: string;
+    fator: string;
+    polinomio: string;
+    unidade: string;
+    alarme: number;
+}
--- src/routes/valores.ts
+++ src/routes/valores.ts
@@ -1,17 +1,57 @@
 import { Router } from "express";
 import type {Request, Response } from "express";
+import { TipoParametroRepository } from "../repository/tipoParametroRepository.ts";
+import { EstacaoRepository } from "../repository/estacaoRepository.ts";
+import { ValorCapturadoRepository } from "../repository/valorCapturadoRepository.ts";
 
 const router = Router();
+const tipoParametroRepository: TipoParametroRepository = new TipoParametroRepository()
+const estacaoRepository: EstacaoRepository = new EstacaoRepository()
+const valorCapturadoRepository: ValorCapturadoRepository = new ValorCapturadoRepository()
 
-router.post("/", (req: Request, res: Response) => {
-    // Recebe qualquer JSON do corpo da requisição
+router.get("/", async (req: Request, res: Response) => {
+    const json_ids = await tipoParametroRepository.getAllJsonIds()
+    const estacoes_uuids = await estacaoRepository.getAllEstacaoUUIDs()
     const dadosRecebidos = req.body;
     
     if (!dadosRecebidos || Object.keys(dadosRecebidos).length === 0) {
       return res.status(400).send({ error: "Nenhum dado foi enviado" });
     }
-    
-    return res.send({ dadosRecebidos });
-  });
+
+    let unixtime: number | null = dadosRecebidos.unixtime;
+    let estacaoId: number | null = estacoes_uuids[dadosRecebidos.UUID];
+
+    if (unixtime == null || estacaoId == null) {
+      return res.status(400).send({ error: "Verifique a existência ou os valores de unixtime e/ou estacaoId" });
+    }
+
+    try {
+      const dadosParaSalvar = [];
+      for (const key in dadosRecebidos) {
+        if (key === "unixtime" || key === "UUID") continue;
+        if (!(key in json_ids)) continue;
+
+        const Parametros_pk = json_ids[key];
+        const valor = dadosRecebidos[key];
+
+        const obj = {
+            unixtime,
+            Parametros_pk,
+            valor,
+            estacao_id: estacaoId
+        };
+
+        dadosParaSalvar.push(obj);
+      }
+      const resultados = await Promise.all(
+        dadosParaSalvar.map(param => valorCapturadoRepository.save(param))
+      );
+    }
+    catch {
+      return res.status(500).json({"error": "Erro no processo de envio para o Back-end"})
+    }
+
+    return res.status(200).send({ "message": "Sucesso no registro dos parâmetros" });
+});
 
 export default router;
